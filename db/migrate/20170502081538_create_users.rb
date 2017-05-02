abe0cdbafb222d2966f107935a3245974ae6548c
salamaashoush <salamaashoush@gmail.com>
2017-05-02 18:08:12 +0200

    merge and fixed conflicts

--- db/migrate/20170502081538_create_users.rb
+++ db/migrate/20170502081538_create_users.rb
@@ -2,7 +2,6 @@ class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       t.string :name
-      t.string :email,              null: false, default: ""
       t.string :password
       t.references :room, foreign_key: true
       t.string :ext
