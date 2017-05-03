f9145592bbf99354d7d8b5a960b3e99b3523a3ba
Ehab Gamal <ehab.g.sh@gmail.com>
2017-05-03 16:05:30 +0200

    Bulding order form ui structure

--- app/assets/javascripts/orders.js
+++ app/assets/javascripts/orders.js
@@ -1,2 +1,19 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+jQuery(document).ready(function() {
+    $.ajax({
+        //url: "/admin/products.json",
+        url: "/rooms.json",
+        success: function(data) {
+            console.log(data);
+        },
+        error: function(error) {
+            console.log(error);
+        }
+    });
+    $('#order_room_id').attr('class','search selection dropdown');
+    $('#product_category_id').attr('class','search selection dropdown');
+    $('#order_user_id').attr('class','search selection dropdown');
+    $('#order_room_id').dropdown();
+    $('#product_category_id').dropdown();
+    $('#order_user_id').dropdown();
+});
+
