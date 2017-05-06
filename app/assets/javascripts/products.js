6683e80fae20eada8ff504baff7473efe6d7a9bd
salamaashoush <salamaashoush@gmail.com>
2017-05-06 22:29:33 +0200

    fixed product update url

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,35 +1,35 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-jQuery(document).ready(function() {
+jQuery(document).ready(function () {
     $('.message .close')
         .on('click', function () {
             $(this).closest('.message').transition('fade');
         });
-        $('.menu a.item, .menu .link.item').on('click',function () {
+    $('.menu a.item, .menu .link.item').on('click', function () {
 
-            $(this)
-                .addClass('active')
-                .closest('.ui.menu')
-                .find('.item')
-                .not($(this))
-                .removeClass('active');
-        });
+        $(this)
+            .addClass('active')
+            .closest('.ui.menu')
+            .find('.item')
+            .not($(this))
+            .removeClass('active');
+    });
 });
 products_table = null;
-jQuery(document).on("turbolinks:before-cache", function() {
+jQuery(document).on("turbolinks:before-cache", function () {
     if (products_table !== null) {
         products_table.fnDestroy();
         products_table = null;
     }
 });
 
-jQuery(document).on("turbolinks:load",function () {
-     if($(".orders.new").length > 0)
-             App.Channels.Products.subscribe()
-    if(!$(".products.index").length > 0){
+jQuery(document).on("turbolinks:load", function () {
+    if ($(".orders.new").length > 0)
+        App.Channels.Products.subscribe()
+    if (!$(".products.index").length > 0) {
         return
     }
-    products_table=$('#products-table').dataTable({
+    products_table = $('#products-table').dataTable({
         "processing": true,
         "serverSide": true,
         "bDestroy": true,
@@ -39,24 +39,24 @@ jQuery(document).on("turbolinks:load",function () {
         // Check dataTables documentation to learn more about
         // available options.
     });
-    products_table.on('draw.dt',function (e) {
+    products_table.on('draw.dt', function (e) {
         $('.ui.checkbox').checkbox();
     });
 
- 
+
 });
 function toggle_available(toggle) {
-    var productId=toggle.dataset.id;
-    var state=(!JSON.parse(toggle.dataset.state))&1;
+    var productId = toggle.dataset.id;
+    var state = (!JSON.parse(toggle.dataset.state)) & 1;
 
     $.ajax({
-        url: '/admin/products/'+productId+'.json',
-        method:'put',
-        data:{product:{available:state}},
-        success:function () {
-            toggle.dataset.state=state;
+        url: '/products/' + productId + '.json',
+        method: 'put',
+        data: {product: {available: state}},
+        success: function () {
+            toggle.dataset.state = state;
         },
-        error:function () {
+        error: function () {
             console.log("error")
         }
     });
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+
   devise_for :users, skip: [:sessions, :registrations]
   as :user do
     get 'login', to: 'devise/sessions#new', as: :new_user_session
@@ -7,11 +9,9 @@ Rails.application.routes.draw do
     get '/sign_up' => 'devise/registrations#new', as: :new_user_registration # custom path to
     post '/sign_up' => 'devise/registrations#create', as: :user_registration
   end
-
   get 'users/:id/orders', to: 'users#orders'
   get 'allproducts.json', to: 'products#all', as: :products_for_user
-
-  resources :categories, :products, :users, :rooms, :orders
+  resources :categories, :products, :users, :rooms,:orders
   get 'checks', to: 'orders#checks', as: :orders_checks
   root :to => 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
